ce1af471d4054ef3ecb498e0ca008064175c1d37
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-06-19 10:44:16 +0200

    Updated dependency script to work on systems where ansible-galaxy is not available

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,9 +2,41 @@
 #
 # Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
 #
-# Install role dependencies using ansible-galaxy
+# Install role dependencies using ansible-galaxy (or manually if needed)
 
-ansible-galaxy install \
-  --ignore-errors \
-  --role-file=ansible/dependencies.yml \
-  --roles-path=ansible/roles/
+# Variables
+ansible_dir=ansible
+role_dir="${ansible_dir}/roles/"
+deps="${ansible_dir}/dependencies.yml"
+
+# Install roles with ansible-galaxy (preferred method)
+galaxy_install() {
+  ansible-galaxy install \
+    --ignore-errors \
+    --role-file=${deps} \
+    --roles-path=${role_dir}
+}
+
+# Install roles from Github (if ansible-galaxy is not available)
+# WARNING: this will only work in quite specific cases
+#  - the roles should be written by me and be available on my github
+#  - the latest version on the master branch should be compatible with the
+#    one specified in dependencies.yml
+manual_install() {
+  local base_url=https://github.com/bertvv/ansible-role-
+  local roles=$(grep src ${deps} | sed 's/.*\.//')
+
+
+  for role in $roles; do
+    git clone "${base_url}${role}.git" "${role_dir}/bertvv.${role}"
+  done
+}
+
+# script proper
+
+if [ -n "$(which ansible-galaxy 2> /dev/null)" ]; then
+  galaxy_install
+else
+  manual_install
+fi
+
